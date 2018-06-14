82c75f1cbd0b5927109537106f5df379a36e6516
JD <j@mesjohnson.com>
2018-06-14 05:20:11 -0700

    Removed Useless Logs

--- server/validation/validateRegistration.js
+++ server/validation/validateRegistration.js
@@ -3,13 +3,12 @@ const validator = require('validator');
 
 module.exports = function validateRegistrationInput(input) {
 
-  const NAME_MIN = 3, NAME_MAX = 30;
   let registrationErrors = {};
 
   // put minimum/maximum limit on characters in name
-  console.log(input.name)
+  const NAME_MIN = 3, NAME_MAX = 30;
+
   if (!validator.isLength(input.name) >= NAME_MIN) {
-    console.log('uhhh')
     registrationErrors.name = `Name must be more than ${NAME_MIN - 1} characters`;
   } else if (!validator.isLength(input.name) <= NAME_MAX) {
     registrationErrors.name = `Name must be less than ${NAME_MAX} characters`;
